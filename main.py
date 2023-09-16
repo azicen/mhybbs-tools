a5ed65b8568aabc1b242e40df4fe9267a63ae80a
null122 <1229608477@qq.com>
2023-09-16 11:07:41 +0800

    feat: 优化获取角色信息输出

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from mhy_game_client.genshin import genshinClient
 
 if __name__ == '__main__':
-    genshinClient.get_user_game_roles()
+    roles = genshinClient.get_user_game_roles("")
+    for r in roles:
+        print(str(r))
--- mhy_game_client/genshin.py
+++ mhy_game_client/genshin.py
@@ -5,7 +5,21 @@ import logging as log
 
 
 class GenshinRoleInfo(RoleInfo):
-    pass
+    def __init__(self, region: str, game_uid: str, nickname: str, region_name: str, level: int):
+        self.region = region
+        self.game_uid = game_uid
+        self.nickname = nickname
+        self.region_name = region_name
+        self.level = level
+
+    def __str__(self):
+        return HttpRequest.toJson({
+            "region": self.region,
+            "game_uid": self.game_uid,
+            "nickname": self.nickname,
+            "region_name": self.region_name,
+            "level": self.level
+        }, ensure_ascii=False)
 
 
 class GenshinIsSignInfo(IsSignInfo):
@@ -17,18 +31,36 @@ class GenshinSignInfo(SignInfo):
 
 
 class GenshinClient(MHYClient):
-    def get_user_game_roles(self, cookie: str) -> GenshinRoleInfo | None:
+    def get_user_game_roles(self, cookie: str) -> list[GenshinRoleInfo] | None:
         header = BaseRequest(cookie)
         response = {}
         try:
-            response = HttpRequest.toPython(req.sendRequest('get', userConfig.GENSHIN_ROLE_URL, headers=header.getHeader()).text)
-            if response is None:
-                return None
+            log.info("获取原神账号信息")
+            response = HttpRequest.toPython(
+                req.sendRequest('get', userConfig.GENSHIN_ROLE_URL, headers=header.getHeader()).text)
+            message = response['message']
         except Exception as e:
             log.error(f'{e}')
             return None
 
+        if response is None:
+            return None
 
+        if response.get('retcode', 1) != 0 or response.get('data', None) is None:
+            log.error(message)
+
+        user_list = []
+        user_info = response.get('data', {}).get('list', [])
+        for user in user_info:
+            user_list.append(GenshinRoleInfo(
+                user['region'],
+                user['game_uid'],
+                user['nickname'],
+                user['region_name'],
+                user['level']
+            ))
+
+        return user_list
 
     def get_sign_state_info(self, cookie: str) -> GenshinIsSignInfo | None:
         pass
